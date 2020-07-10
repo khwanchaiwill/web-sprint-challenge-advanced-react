0ebea832bb8b5ea3a964ba26b5558454cbd68dd3
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2020-07-10 13:52:38 -0400

    finished-MVP

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,46 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+});
+
+test("form shows success message on submit with form details", () => {
+    const {getByText, getByLabelText, getByTestId} = render(<CheckoutForm/>)
+
+    const h2 = getByText(/checkout form/i);
+    const firstName = getByLabelText(/first name/i);
+    const lastName = getByLabelText(/last name/i);
+    const address = getByLabelText(/address/i);
+    const city = getByLabelText(/city/i);
+    const state = getByLabelText(/state/i);
+    const zip = getByLabelText(/zip/i);
+
+});
+test("handle input changes on submit form", () => {
+    const { getByLabelText} = render(<CheckoutForm/>)
+
+    const firstName = getByLabelText(/first name/i);
+    const lastName = getByLabelText(/last name/i);
+    const address = getByLabelText(/address/i);
+    const city = getByLabelText(/city/i);
+    const state = getByLabelText(/state/i);
+    const zip = getByLabelText(/zip/i);
+
+    fireEvent.change(firstName, {target: {value: 'Khwanchai'} });
+    fireEvent.change(lastName, {target: {value: 'Phaipha'} });
+    fireEvent.change(address, {target: {value: '2728 Tudor Rd'} });
+    fireEvent.change(city, {target: {value: 'columbus'} });
+    fireEvent.change(state, {target: {value: 'ohio'} });
+    fireEvent.change(zip, {target: {value: 43209 } });
+
+})
+test("Submit button", () => {
+    const { getByText} = render(<CheckoutForm/>)
+    const submit = {button: 0}
+    fireEvent.click(getByText('Checkout'), submit)
+})
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,15 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react'
+
+export const useForm = initialValues => {
+    const [valuse, setValues] = useState(initialValues);
+
+
+    const handleChanges = e =>{
+        setValues({
+            ...valuse, [e.target.name]: e.target.value
+        });
+    }
+ 
+    return [valuse, handleChanges]
+}
